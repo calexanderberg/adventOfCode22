ffe8b5d0863f6067e82cee11d0c14ac92f121a6d
calexanderberg <carlalexanderberg@gmail.com>
2022-12-11 12:16:06 +0100

    Part 1 done

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -1,5 +1,7 @@
+use std::collections::HashSet;
+
 fn main() {
-    let file = std::fs::read_to_string("test.txt").unwrap();
+    let file = std::fs::read_to_string("input.txt").unwrap();
     
     let mut head_vec: Vec<Vec<i32>> = vec![vec![0,0]];
 
@@ -9,6 +11,7 @@ fn main() {
         let new_value = lines[1].parse::<i32>().unwrap();
         let mut current_val = head_vec[head_vec.len()-1].clone();
 
+        // Creating our head path
         match lines[0] {
             "U" =>{
                 for _a in 1..new_value+1 {
@@ -46,32 +49,42 @@ fn main() {
         }
     }
 
-    println!("{:?}", head_vec);
-    let sum = tail_trail(head_vec, vec![vec![0,0]], 0,1, 0);
+    let sum = tail_trail(head_vec, vec![vec![0,0]], 0,1);
+
     println!("Sum: {:?}", sum);
 }
 
-fn tail_trail(vector: Vec<Vec<i32>>, mut tail: Vec<Vec<i32>>, current_head: usize , next_head: usize, sum: u32) -> u32 {
+fn tail_trail(vector: Vec<Vec<i32>>, mut tail: Vec<Vec<i32>>, prev_head: usize , next_head: usize) -> usize {
 
     let temp_head = vector[next_head].clone();
     let temp_tail = tail.clone().pop().unwrap();
 
-
-
-    println!("head: {:?}", vector[current_head]);
-    println!("tail: {:?}", tail.clone().pop().unwrap());
-    println!("next head: {:?}\n", vector[next_head]);
+    if !is_neightbor(temp_tail, temp_head) {
+       tail.push(vector[prev_head].clone()); 
+    } 
   
-
-    if vector[next_head] != vector[vector.len()-1] {
-        tail_trail(vector, tail, next_head, next_head+1, sum);
+    if next_head != vector.len()-1 {
+        return tail_trail(vector, tail, next_head, next_head+1);
     } else {
-        //return sum;
+        let set: HashSet<Vec<i32>> = tail.into_iter().collect();
+        return set.len();
     }
-    return 0;
 }
 
-fn is_neightbor(){
-    
+fn is_neightbor(host: Vec<i32>, client: Vec<i32>) -> bool {
+    return 2 as f32 > (square(client[0]-host[0]) as f32 + square(client[1]-host[1]) as f32).sqrt()
 }
  
+fn square(value: i32) -> i32 {
+    return value * value;
+}
+
+/*
+fn last_tail(parent: Vec<i32>, child: Vec<i32>, i: u32) -> <i32> {
+    if i > 8 {
+        return vector;
+    } else {
+
+    }
+}
+ */
