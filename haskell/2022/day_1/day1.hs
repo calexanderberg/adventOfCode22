7a622af61c8c57446725ca4d2edffdd6b369839f
calexanderberg <carlalexanderberg@gmail.com>
2023-08-10 14:54:41 +0200

    Fix: rename day_1

---
+++ haskell/2022/day_1/day1.hs
@@ -0,0 +1,16 @@
+module Day1 where
+
+import Data.List (sort)
+
+countCal :: [String] -> Int -> [Int]
+countCal [] _ = []
+countCal ("" : t) val = val : countCal t 0
+countCal (h : t) val = countCal t (val + read h)
+
+part1 :: String -> Int
+part1 file = maximum (countCal (lines file) 0)
+
+part2 :: String -> Int
+part2 file = sum (drop (length list - 3) list)
+  where
+    list = sort $ countCal (lines file) 0
