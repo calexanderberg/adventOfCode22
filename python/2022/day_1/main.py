a17306b95eb2b6c5e2c4f9ccad496b749fc99688
calexanderberg <carlalexanderberg@gmail.com>
2023-08-16 19:14:19 +0200

    Fix: optimization, i got bored

--- python/2022/day_1/main.py
+++ python/2022/day_1/main.py
@@ -2,11 +2,12 @@ def count_cal(strings, val):
     result = []
     for s in strings:
         if s == "":
-            result.append(val)
             val = 0
         else:
             val += int(s)
-    return sorted(result, reverse=True)
+        result.append(val)
+    result.sort(reverse=True)
+    return result
 
 
 def part1(file): return sum(count_cal(read_lines(file), 0)[:1])
