f068a0fec56395b6e47358cc90d49dd8cbc01f8a
21st <60197499+21st-centuryman@users.noreply.github.com>
2023-08-16 19:18:43 +0200

    Cut: remove print

--- python/2022/day_1/main.py
+++ python/2022/day_1/main.py
@@ -13,7 +13,3 @@ def count_cal(strings, val):
 def part1(file): return sum(count_cal(read_lines(file), 0)[:1])
 def part2(file): return sum(count_cal(read_lines(file), 0)[:3])
 def read_lines(filename): return [line.strip() for line in open(filename)]
-
-
-print(part1("input.txt"))
-print(part2("input.txt"))
