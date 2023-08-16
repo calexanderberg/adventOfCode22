2f7c806d91fddf001c9b9af136ed55a643e217ab
calexanderberg <carlalexanderberg@gmail.com>
2023-08-16 11:10:19 +0200

    Fix: py 22 day1

--- python/2022/day_1/main.py
+++ python/2022/day_1/main.py
@@ -1,26 +1,18 @@
-def create_list(filename):
-    elf_list = []
-    current_elf = 0
+def count_cal(strings, val):
+    result = []
+    for s in strings:
+        if s == "":
+            result.append(val)
+            val = 0
+        else:
+            val += int(s)
+    return sorted(result, reverse=True)
+
+
+def part1(file): return sum(count_cal(read_lines(file), 0)[:1])
+def part2(file): return sum(count_cal(read_lines(file), 0)[:3])
+
+
+def read_lines(filename):
     with open(filename) as file:
-        for line in file:
-            line = line.strip()
-            if line:
-                current_elf += int(line)
-            else:
-                elf_list.append(current_elf)
-                current_elf = 0
-    elf_list.sort()
-    return elf_list
-
-
-def most_cal(lst, number):
-    return 0 if number == 0 else lst[-number] + most_cal(lst, number - 1)
-
-
-def part1(str): return most_cal(create_list(str), 1)
-def part2(str): return most_cal(create_list(str), 3)
-
-
-input = "input.txt"
-print("Most cal for 1 elf: ", part1(input))
-print("Most cal for 3 elves: ", part2(input))
+        return [line.strip() for line in file]
