49c0136ff3a112ba065378be89f18dfc79c8140e
calexanderberg <carlalexanderberg@gmail.com>
2023-08-19 19:49:32 +0200

    Add day 8 part 1

--- haskell/2022/day_8/main.hs
+++ haskell/2022/day_8/main.hs
@@ -1,18 +1,32 @@
 module Main where
 
+import Data.Bits ((.|.))
 import Data.List (transpose)
 
-numVisible :: [String] -> Int
-numVisible (x : xs) = 1
+buildMatrix :: [String] -> [[Int]]
+buildMatrix = map (map (read . pure))
 
-checkAllDirections :: [String] -> Int
-checkAllDirections l = sum $ map numVisible [l, reverse l, transpose l, reverse (transpose l)]
+seenItem :: Int -> [Int] -> [Int]
+seenItem _ [] = []
+seenItem a (x : xs)
+  | a < x = 1 : seenItem x xs
+  | otherwise = 0 : seenItem a xs
+
+seenLines :: [[Int]] -> [[Int]]
+seenLines = map (\x -> zipWith max (seenItem (-1) x) $ reverse $ seenItem (-1) $ reverse x)
+
+seenMatrix :: [String] -> [[Int]]
+seenMatrix str = zipWith (zipWith (.|.)) (seenLines $ buildMatrix str) $ reverse . transpose $ seenLines $ transpose . reverse $ buildMatrix str
+
+countSeen :: [[Int]] -> Int
+countSeen matrix = sum [1 | x <- concat matrix, x == 1]
 
 part1 :: String -> Int
-part1 file = checkAllDirections (lines file)
+part1 = countSeen . seenMatrix . lines
 
 main :: IO ()
 main = do
-  file <- readFile "test.txt"
-  putStrLn $ "Part 1: " ++ show (part1 file)
-  putStrLn $ "Part 2: " ++ show (part1 file)
+  file <- readFile "input.txt"
+  print $ part1 file
+
+-- putStrLn $ "Part 2: " ++ show (part1 file)
