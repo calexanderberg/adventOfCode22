7c245b04d8a286d3f7e48d11409080c04fc40e6f
calexanderberg <carlalexanderberg@gmail.com>
2023-10-08 19:21:03 +0200

    Add: half finished rust code

---
+++ rust/aoc2022/src/day7/mod.rs
@@ -0,0 +1,29 @@
+pub fn calc_dir_value(list: Vec<&str>, command: Vec<&str>, val: i32, dir: &str) -> Vec<i32> {}
+
+pub fn nav_to(list: Vec<&str>, command: Vec<&str>, current: &str, goal: &str) -> Vec<i32> {
+    if command.is_empty() {
+        return nev_to(list, list, vec!["/"], goal);
+    }
+    if current == goal {
+        return calc_dir_value(list, command[1..], 0, current);
+    }
+    if {
+
+    } else {
+        return nav_to(list, command[1..], current, goal);
+    }
+}
+
+pub fn create_list(input: &str) -> Vec<i32> {
+    return calc_dir_value(input[1..], input[1..], 0, "/");
+}
+
+pub fn part1(file: &str) -> i32 {
+    let input: Vec<String> = file.lines().map(|s| s.to_string()).collect();
+    return create_list(&input).iter().sum();
+}
+
+//pub fn part2(file: &str) -> i32 {
+//    let input: Vec<String> = file.lines().map(|s| s.to_string()).collect();
+//    return num_contain(&input, 2);
+//}
--- rust/aoc2022/src/main.rs
+++ rust/aoc2022/src/main.rs
@@ -6,6 +6,7 @@ mod day3;
 mod day4;
 //mod day5;
 mod day6;
+mod day7;
 
 fn main() {
     let test = fs::read_to_string("./tests/day6.txt").unwrap();
