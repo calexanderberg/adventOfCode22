edab5b9869e48bcd9b3ac7f59b1bd5dd2bef8bd8
calexanderberg <carlalexanderberg@gmail.com>
2023-10-08 22:06:19 +0200

    Add: tinygrad to pytorch

--- pytorch/2022/day1/lib.py
+++ pytorch/2022/day1/lib.py
@@ -19,3 +19,30 @@ def read_lines(filename): return [line.strip() for line in open(filename)]
 file = "../../inputs/day1.txt"
 print(part1(file))
 print(part2(file))
+
+
+# ---------------------------------
+#          TINYGRAD
+# ---------------------------------
+# import numpy as np
+# from tinygrad.tensor import Tensor
+#
+#
+# def cal_tensor(input):
+#    tensor = Tensor([elf + [0] * (max(len(elf)  # max = elf w/ most items
+#                    for elf in input) - len(elf)) for elf in input])
+#    return sorted(np.sum(tensor.numpy(), axis=1), reverse=True)
+#
+#
+# def part1(file): return int(cal_tensor(read_lines(file))[0])
+# def part2(file): return int(sum(cal_tensor(read_lines(file))[:3]))
+#
+#
+# def read_lines(filename):
+#    with open(filename, "r") as file:
+#        return [[int(cal) for cal in line.split()] for line in file.read().split("\n\n")]
+#
+#
+# file = "../../tests/day1.txt"
+# print(part1(file))
+# print(part2(file))
