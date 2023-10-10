0ce1dfe0ba9bcd6019abeb424f4e14dd062832ed
21st <60197499+21st-centuryman@users.noreply.github.com>
2023-10-10 10:07:06 +0200

    Add: torch.py

---
+++ pytorch/2022/day1/torch.py
@@ -0,0 +1,21 @@
+import torch
+
+
+def cal_tensor(input):
+    tensor = torch.tensor([elf + [0] * (max(len(elf)for elf in input)  # max = elf w/ most items
+                                        - len(elf)) for elf in input])
+    return torch.sort(torch.sum(tensor, dim=1), descending=True)[0].numpy()
+
+
+def part1(file): return int(cal_tensor(read_lines(file))[0])
+def part2(file): return int(sum(cal_tensor(read_lines(file))[:3]))
+
+
+def read_lines(filename):
+    with open(filename, "r") as file:
+        return [[int(cal) for cal in line.split()] for line in file.read().split("\n\n")]
+
+
+file = "../inputs/day1.txt"
+print(part1(file))
+print(part2(file))
