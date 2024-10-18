951759994e36f604221286e4f95ef34ca5c68a93
calexanderberg <carlalexanderberg@gmail.com>
2024-10-18 17:17:27 +0200

    Add: tinygrad restructure

--- tinygrad/tiny.py
+++ tinygrad/tiny.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 def tiny_days():
-    subprocess.run(["python3", f"./{sys.argv[1]}/tiny.py"])
+    subprocess.run(["python3", f"./{sys.argv[1]}}/{sys.argv[2]}/tiny.py"])
 
 if __name__ == "__main__":
     tiny_days()
