a27d27c1e005b3b93a2244f84a8472de03263782
calexanderberg <carlalexanderberg@gmail.com>
2024-10-19 10:53:13 +0200

    Tests

--- rust/2022/day1/rust.rs
+++ rust/2022/day1/rust.rs
@@ -17,7 +17,7 @@ pub fn part2(file: &str) -> i32 {
 }
 
 fn main() {
-    let input = std::fs::read_to_string("./tests/day1.txt").unwrap();
+    let input: &str = std::fs::read_to_string("./inputs/day1.txt").unwrap();
     assert!(part1(input) == 68802,"Part1 is not working");
     assert!(part2(input) == 205370,"Part2 is not working");
 }
